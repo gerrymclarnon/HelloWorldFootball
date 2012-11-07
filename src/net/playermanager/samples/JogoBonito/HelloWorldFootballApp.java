614775c79c4eb228dea78dfca03eae779ed0b190
playermanager <gerry@mclarnonworld.com>
2012-11-07 12:59:49 +0000

    Initial commit

---
+++ src/net/playermanager/samples/JogoBonito/HelloWorldFootballApp.java
@@ -0,0 +1,7 @@
+package net.playermanager.samples.JogoBonito;
+
+public class HelloWorldFootballApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World Football");
+    }
+}
